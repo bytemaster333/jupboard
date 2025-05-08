9862ec3c7d2d6022c27913d3802cbabec623aded
ByteMaster <115087065+bytemaster333@users.noreply.github.com>
2025-05-08 09:38:50 +0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zreknaqtjijmmcklpboh.supabase.co'; // Supabase dashboard > Project Settings > API
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpyZWtuYXF0amlqbW1ja2xwYm9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MjYyOTIsImV4cCI6MjA2MjIwMjI5Mn0.cVskeVrieXGumO02GDbRLIwU9w-c22WUKuLb59OazZc'; // Supabase dashboard > Project Settings > API > anon public key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
